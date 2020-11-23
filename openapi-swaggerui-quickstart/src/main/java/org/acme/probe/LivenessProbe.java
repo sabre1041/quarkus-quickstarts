1deb79dbc873477f67ed4000f393e448bfd957ab
Andrew Block <andy.block@gmail.com>
2020-11-23 10:38:46 -0600

    Modifications to support swagger-ui and smallrye

---
+++ openapi-swaggerui-quickstart/src/main/java/org/acme/probe/LivenessProbe.java
@@ -0,0 +1,18 @@
+package org.acme.probe;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+@ApplicationScoped
+public class LivenessProbe implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("I'm still alive!");
+    }
+    
+}
---
+++ openapi-swaggerui-quickstart/src/main/java/org/acme/probe/ReadinessProbe.java
@@ -0,0 +1,18 @@
+package org.acme.probe;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+@Readiness
+@ApplicationScoped
+public class ReadinessProbe implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("I'm ready!");
+    }
+    
+}
